0e1e81cfca1bc8290101699404eb084ec3f781f1
haha <haha@itcast.com>
2019-06-30 19:14:20 +0800

    第一次提交

---
+++ haha/src/cn/itcast/test/Hello.java
@@ -0,0 +1,11 @@
+package cn.itcast.test;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈");
+        System.out.println("哈哈哈哈");
+        System.out.println("哈哈哈哈");
+        System.out.println("哈哈哈哈");
+    }
+}
